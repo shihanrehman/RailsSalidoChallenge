7f79ae388d3eab4e2a38b1d953a2118b46e6861c
Shihan Rehman <shihanrehman@gmail.com>
2016-01-23 20:07:59 -0500

    Configured Postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160123052824) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "posts", force: :cascade do |t|
     t.string   "name"
     t.decimal  "price"

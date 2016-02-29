c1f046fc215e18123b4e73a5022e5b2ee3ffaa41
Shihan Rehman <shihanrehman@gmail.com>
2016-02-29 03:43:52 -0500

    Added comments to api requests

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,16 +1,16 @@
 class Api::PostsController < ApplicationController
 	skip_before_filter :verify_authenticity_token
 	def index
-		render json: Post.all  #localhost:3000/api/posts.json
+		render json: Post.all  #curl localhost:3000/api/posts.json
 	end
 
 	def show
 		post = Post.find(params[:id])
-		render json: post
+		render json: post #curl http://localhost:3000/api/psons/1.json
 	end
 
 	def create
-		post  = Post.new(list_params)
+		post  = Post.new(list_params)  #curl -i -H "Accept: application/json" -H "Content-type:application/json" -X POST -d '{"name":"Martini"}' http://localhost:3000/api/posts
 		 if post.save
 		      render json: {
 		      status: 200,
@@ -20,10 +20,32 @@ class Api::PostsController < ApplicationController
 		 else
 		    render json: {
 		      status: 500,
-		      errors: list.errors
+		      errors: post.errors
 		    }.to_json
 		 end
 	end
+
+	def update
+		post = Post.find(params[:id])	#curl -i -H  "Content-type:application/json" -X PUT -d '{"name":"Vodka1"}' http://localhost:3000/api/posts/15.json
+		if post.update(list_params)
+			render json: {
+				status: 200,
+				message: "Successfully updated",
+				post: post
+			}.to_json
+		end
+	end
+
+
+	def destroy
+		post = Post.find(params[:id])	#curl -i -H "Accept: application/json" -H "Content-type:application/json" -X DELETE -d '{"name":""}' http://localhost:3000/api/posts/12.json
+		post.destroy
+		render json: {
+			status: 200,
+			message: "Successfully deleted post"
+			}.to_json
+	end
+
 
 	private
 		def list_params 
@@ -36,6 +58,7 @@ end
 #In terminal, run curl http://localhost:3000/api/posts.json
 #show, curl http://localhost:3000/api/posts/4.json
 #curl -i -H 
+
 
 =begin 
 curl \
